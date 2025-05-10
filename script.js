1b2ffd0f9bdbbc9e0bff5b8b80ec5de7298d69ee
Shiva Dhanush <dhanushshiva2003@gmail.com>
2025-05-10 14:27:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+function display(a){
+    let data = document.getElementById("disp")
+    if(data.innerHTML == '0'){
+        data.innerHTML = a
+    }else{
+        data.innerHTML = data.innerHTML + a
+    }
+}
+
+function u_clear(){
+    let data = document.getElementById("disp")
+    data.innerHTML = 0   
+}
+
+function calculate(){
+    let data = document.getElementById("disp")
+    let expression = data.innerHTML
+    let res = eval(expression)
+    console.log(res)
+    data.innerHTML = res
+    
+}
